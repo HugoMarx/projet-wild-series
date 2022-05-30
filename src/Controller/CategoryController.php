7f9b140686989021f767ea5e6b8e6cc9d0cb4bf4
Hugo Marx <marx.hugo@gmail.com>
2022-05-30 10:38:05 +0200

    Form add category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,22 +2,45 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
 use App\Repository\CategorieRepository;
 use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\CategoryType;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/category', name: 'category_')]
 class CategoryController extends AbstractController
 {
+
+    #[Route('/new', name: 'new')]
+    public function new(Request $request, CategorieRepository $categorieRepository)
+    {
+        $category = new Categorie;
+
+        $form = $this->createForm(CategoryType::class, $category);
+        
+        $form->handleRequest($request);
+       
+        if($form->isSubmitted()){
+            $categorieRepository->add($category, true);      
+        }
+
+        // Render the form (best practice)
+        return $this->renderForm('category/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
     #[Route('/', name: 'index')]
     public function index(CategorieRepository $categorieRepository, ProgramRepository $programRepository): Response
     {
         $categories = $categorieRepository->findAll();
         $itemByCategory = [];
-        
-       foreach($categories as $item){
+
+        foreach ($categories as $item) {
             $itemByCategory[$item->getName()] = count($item->getPrograms());
         }
 
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use App\Entity\Episode;
 use App\Entity\Program;
 use App\Entity\Season;
+use App\Form\ProgramType;
 use App\Repository\EpisodeRepository;
 use App\Repository\ProgramRepository;
 use App\Repository\SeasonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -26,10 +28,27 @@ class ProgramController extends AbstractController
         ]);
     }
 
+    #[Route('/new', name: 'new')]
+    public function new(Request $request, ProgramRepository $programRepository)
+    {
+        $program = new Program();
+        $form = $this->createForm(ProgramType::class, $program);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+            $programRepository->add($program, true);
+        }
+
+
+        return $this->renderForm('program/new.html.twig', [
+            'form' => $form]);
+    }
+
+
     #[Route('/show/{id<^[0-9]+$>}', name: 'show')]
     public function show(Program $program, SeasonRepository $seasonRepository): Response
     {
-       
+
         $seasons = $seasonRepository->findByProgram($program);
 
 
@@ -48,11 +67,10 @@ class ProgramController extends AbstractController
     #[Route('/{program}/seasons/{season}', name: 'season_show')]
     public function showSeason(
         Program $program,
-        Season $season,  
+        Season $season,
         EpisodeRepository $episodeRepository
 
-    ): Response
-    {
+    ): Response {
         $episodes = $episodeRepository->findBySeason($season, ['number' => 'ASC']);
         return $this->render('program/season_show.html.twig', [
             'program' => $program,
@@ -62,7 +80,8 @@ class ProgramController extends AbstractController
     }
 
     #[Route('/{program}/seasons/{season}/episode/{episode}', name: 'episode_show')]
-    public function showEpisode(Program $program, Season $season, Episode $episode){
+    public function showEpisode(Program $program, Season $season, Episode $episode)
+    {
         return $this->render('program/episode_show.html.twig', [
             'program' => $program,
             'season' => $season,
